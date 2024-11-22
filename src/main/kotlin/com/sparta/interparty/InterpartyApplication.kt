aac22e85aa49bcfdaca28f86eaf1bd8d21adaa89
coldrice99 <coldrice99@gmail.com>
2024-11-22 20:45:04 +0900

    init commit

---
+++ src/main/kotlin/com/sparta/interparty/InterpartyApplication.kt
@@ -0,0 +1,11 @@
+package com.sparta.interparty
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InterpartyApplication
+
+fun main(args: Array<String>) {
+	runApplication<InterpartyApplication>(*args)
+}
