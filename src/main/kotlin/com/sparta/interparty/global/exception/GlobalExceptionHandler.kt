705809784e45a2a932f58fc0c168269b1966c9b6
34-43 <jehyuck3443@gmail.com>
2024-11-25 13:07:00 +0900

    feat: 전역 예외 처리기 구현 #8

---
+++ src/main/kotlin/com/sparta/interparty/global/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,29 @@
+package com.sparta.interparty.global.exception
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    // 처리되지 않은 모든 RuntimeException 상속체에 대한 처리
+    @ExceptionHandler(RuntimeException::class)
+    fun handleRuntimeException(ex: RuntimeException): ResponseEntity<ExceptionResponse> {
+        return ExceptionResponseStatus.INTERNAL_SERVER_ERROR.toResponseEntity()
+    }
+
+    // 사용자 지정 예외 클래스에 대한 처리
+    @ExceptionHandler(CustomException::class)
+    fun handleCustomException(ex: CustomException): ResponseEntity<ExceptionResponse> {
+        return ex.exceptionResponseStatus.toResponseEntity()
+    }
+
+    // 유효성 검사 예외의 처리
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleException(ex: MethodArgumentNotValidException): ResponseEntity<ExceptionResponse> {
+        val msg: String = ex.bindingResult.allErrors[0].defaultMessage?: "입력 값이 유효하지 않습니다."
+        return ExceptionResponseStatus.BAD_REQUEST.toResponseEntity(msg)
+    }
+}
