40bad782e91ca46d2924fce8e0fa3fa4082e92f6
coldrice99 <coldrice99@gmail.com>
2024-11-25 20:13:55 +0900

    #14 refactor: SigninReqDto 수정(로그인 오류 fix)

--- src/main/kotlin/com/sparta/interparty/domain/auth/dto/req/SigninReqDto.kt
+++ src/main/kotlin/com/sparta/interparty/domain/auth/dto/req/SigninReqDto.kt
@@ -2,11 +2,10 @@ package com.sparta.interparty.domain.auth.dto.req
 
 import jakarta.validation.constraints.NotBlank
 
-
 data class SigninReqDto(
     @field:NotBlank
-    val username: String,
+    val username: String = "",
 
     @field:NotBlank
-    val password: String
+    val password: String = ""
 )
