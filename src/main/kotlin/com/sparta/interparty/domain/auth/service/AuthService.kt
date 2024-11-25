d9117aa6adee1e74c47ebd6e00eca7259243c209
coldrice99 <coldrice99@gmail.com>
2024-11-25 12:28:07 +0900

    #2 feat: auth 도메인 추가

--- src/main/kotlin/com/sparta/interparty/domain/auth/service/AuthService.kt
+++ src/main/kotlin/com/sparta/interparty/domain/auth/service/AuthService.kt
@@ -1,11 +1,10 @@
 package com.sparta.interparty.domain.auth.service
 
-import com.sparta.interparty.domain.auth.dto.req.SignupRequestDto
+import com.sparta.interparty.domain.auth.dto.req.SignupReqDto
 import com.sparta.interparty.domain.user.entity.User
 import com.sparta.interparty.domain.user.entity.UserRole
 import com.sparta.interparty.domain.user.repo.UserRepository
-import com.sparta.interparty.global.exception.DuplicateEmailException
-import com.sparta.interparty.global.exception.DuplicateUsernameException
+import com.sparta.interparty.global.exception.CustomException
 import com.sparta.interparty.global.exception.ExceptionResponseStatus
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
@@ -15,14 +14,14 @@ class AuthService(
     private val userRepository: UserRepository,
     private val passwordEncoder: PasswordEncoder,
 ) {
-    fun signup(signupRequestDto: SignupRequestDto) {
+    fun signup(signupRequestDto: SignupReqDto) {
 
         if(userRepository.existsByUsername(signupRequestDto.username)){
-            throw DuplicateUsernameException(ExceptionResponseStatus.DUPLICATE_USERNAME)
+            throw CustomException(ExceptionResponseStatus.DUPLICATE_USERNAME)
         }
 
         if(userRepository.existsByEmail(signupRequestDto.email)){
-            throw DuplicateEmailException(ExceptionResponseStatus.DUPLICATE_EMAIL)
+            throw CustomException(ExceptionResponseStatus.DUPLICATE_EMAIL)
         }
 
         val encodedPassword = passwordEncoder.encode(signupRequestDto.password)
