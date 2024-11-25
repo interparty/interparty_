3fce6df31a2ccba74d0c0855aa5c987cded01f5e
34-43 <jehyuck3443@gmail.com>
2024-11-25 13:06:43 +0900

    feat: 사용자 지정 예외 Response 추가 #8

---
+++ src/main/kotlin/com/sparta/interparty/global/exception/ExceptionResponse.kt
@@ -0,0 +1,11 @@
+package com.sparta.interparty.global.exception
+
+import org.springframework.http.HttpStatus
+
+data class ExceptionResponse(
+    val status: Int,
+    val message: String
+) {
+    constructor(exceptionResponseStatus: ExceptionResponseStatus) : this(exceptionResponseStatus.status.value(), exceptionResponseStatus.message)
+    constructor(status: HttpStatus, message: String) : this(status.value(), message)
+}
