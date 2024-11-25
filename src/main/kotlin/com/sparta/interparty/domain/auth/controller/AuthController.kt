b4713c82bdcf246a0722404f19ba548b7b58638f
coldrice99 <coldrice99@gmail.com>
2024-11-25 12:28:07 +0900

    #2 feat: auth 도메인 추가

--- src/main/kotlin/com/sparta/interparty/domain/auth/controller/AuthController.kt
+++ src/main/kotlin/com/sparta/interparty/domain/auth/controller/AuthController.kt
@@ -1,4 +1,23 @@
 package com.sparta.interparty.domain.auth.controller
 
-class AuthController {
+import com.sparta.interparty.domain.auth.dto.req.SignupRequestDto
+import com.sparta.interparty.domain.auth.service.AuthService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/auth")
+class AuthController(
+    private val authService: AuthService
+) {
+
+    @PostMapping("/signup")
+    fun signup(@RequestBody signupRequestDto: SignupRequestDto): ResponseEntity<String> {
+        authService.signup(signupRequestDto)
+        return ResponseEntity.status(HttpStatus.CREATED).body("회원 가입이 완료되었습니다!")
+    }
 }
