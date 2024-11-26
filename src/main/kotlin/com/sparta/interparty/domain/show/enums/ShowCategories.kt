34d1ecec07b9c3dba0897f1dd51911fd89cca237
34-43 <jehyuck3443@gmail.com>
2024-11-26 10:38:30 +0900

    #3 feat: 'Show' 도메인 관련 열거형 추가 & 수정

---
+++ src/main/kotlin/com/sparta/interparty/domain/show/enums/ShowCategories.kt
@@ -0,0 +1,10 @@
+package com.sparta.interparty.domain.show.enums
+
+enum class ShowCategories {
+    CONCERT,
+    THEATER,
+    MUSICAL,
+    EXHIBITION,
+    SPORTS,
+    ETC
+}
--- src/main/kotlin/com/sparta/interparty/global/exception/ExceptionResponseStatus.kt
+++ src/main/kotlin/com/sparta/interparty/global/exception/ExceptionResponseStatus.kt
@@ -15,12 +15,19 @@ enum class ExceptionResponseStatus(val status: HttpStatus, val message: String) 
     // auth
     DUPLICATE_USERNAME(HttpStatus.CONFLICT,"이미 존재하는 계정 이름입니다."), // 409
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
-    INVALID_USERROLE(HttpStatus.BAD_REQUEST, "유효하지 않은 사용자 권한입니다.")
+    INVALID_USERROLE(HttpStatus.BAD_REQUEST, "유효하지 않은 사용자 권한입니다."),
+    LOGIN_REQUIRED(HttpStatus.UNAUTHORIZED, "로그인이 필요한 서비스입니다."),
+
     // reservation
 
     // review
 
     // show
+    MANAGER_ROLE_REQUIRED(HttpStatus.FORBIDDEN, "매니저 권한이 필요한 서비스입니다."), // 403
+    SHOW_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 공연 정보를 찾을 수 없습니다."), // 404
+    NO_FIELD_MODIFIED(HttpStatus.BAD_REQUEST, "수정할 필드가 없습니다."), // 400
+    INVALID_FIELD_MODIFICATION(HttpStatus.BAD_REQUEST, "잘못된 값으로 수정을 시도중입니다."), // 400
+    NOT_A_MANAGER(HttpStatus.FORBIDDEN, "해당 공연의 매니저가 아닙니다."), // 403
 
     // user
 
