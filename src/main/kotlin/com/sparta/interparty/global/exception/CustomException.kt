f6b8594bc9e44eaff9c6ab6a11c64e05957417f7
KIMHYEJIN <0411khj@naver.com>
2024-11-26 11:41:02 +0900

    feat: 리뷰 관련 예외 처리 로직 추가 - ExceptionResponseStatus에 리뷰 관련 상태(REVIEW_NOT_FOUND, REVIEW_ALREADY_EXISTS, INVALID_REVIEW_REQUEST) 추가 - CustomException을 통해 도메인별 예외 처리 구조 구현 - ExceptionResponse를 활용하여 클라이언트로 명확한 응답 반환

--- src/main/kotlin/com/sparta/interparty/global/exception/CustomException.kt
+++ src/main/kotlin/com/sparta/interparty/global/exception/CustomException.kt
@@ -2,3 +2,7 @@ package com.sparta.interparty.global.exception
 
 class CustomException(val exceptionResponseStatus: ExceptionResponseStatus) :
     RuntimeException(exceptionResponseStatus.message)
+
+// auth
+class DuplicateUsernameException(val exceptionResponseStatus: ExceptionResponseStatus) :
+    RuntimeException(exceptionResponseStatus.message)
