5a94cf9c4eb75a378815874263c8462991ed3e25
coldrice99 <coldrice99@gmail.com>
2024-11-26 11:36:18 +0900

    #17 refactor: 회원탈퇴 기능 추가

---
+++ src/main/kotlin/com/sparta/interparty/domain/user/dto/res/SignoutResDto.kt
@@ -0,0 +1,5 @@
+package com.sparta.interparty.domain.user.dto.res
+
+data class SignoutResDto(
+    val okDelete: String
+)
--- src/main/kotlin/com/sparta/interparty/domain/user/service/UserService.kt
+++ src/main/kotlin/com/sparta/interparty/domain/user/service/UserService.kt
@@ -1,13 +1,20 @@
 package com.sparta.interparty.domain.user.service
 
+import com.sparta.interparty.domain.user.dto.req.SignoutReqDto
 import com.sparta.interparty.domain.user.dto.res.UserResDto
+import com.sparta.interparty.domain.user.repo.UserRepository
 import com.sparta.interparty.global.exception.CustomException
 import com.sparta.interparty.global.exception.ExceptionResponseStatus
 import com.sparta.interparty.global.security.UserDetailsImpl
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
-class UserService {
+class UserService(
+    private final val userRepository: UserRepository,
+    private final val passwordEncoder: PasswordEncoder
+) {
+
     fun getUserInfo(userDetails: UserDetailsImpl): UserResDto {
         if (userDetails.getUser()!!.isDeleted) {
             throw CustomException(ExceptionResponseStatus.DELETED_USER)
@@ -22,5 +29,14 @@ class UserService {
             phoneNumber = user.phoneNumber,
             userRole = user.userRole.toString()
         )
+    }
+
+    fun signout(userDetails: UserDetailsImpl, req: SignoutReqDto) {
+        if (!passwordEncoder.matches(req.password, userDetails.password)) {
+            throw CustomException(ExceptionResponseStatus.INVALID_PASSWORD)
+        }
+        val user = userDetails.getUser() ?: throw CustomException(ExceptionResponseStatus.USER_NOT_FOUND)
+        user.isDeleted = true
+        userRepository.save(user)
     }
 }
