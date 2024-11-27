07faa174cf777cf59c8c728dc19dad213ebd7ea4
34-43 <jehyuck3443@gmail.com>
2024-11-27 16:17:19 +0900

    #28 fix: 'Reservation' 예매 Dto 유효성 조건 추가

--- src/main/kotlin/com/sparta/interparty/domain/reservation/dto/req/ReservationReqDto.kt
+++ src/main/kotlin/com/sparta/interparty/domain/reservation/dto/req/ReservationReqDto.kt
@@ -1,7 +1,10 @@
 package com.sparta.interparty.domain.reservation.dto.req
 
-import java.util.UUID
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
 
-class ReservationReqDto (
+data class ReservationReqDto (
+    @field:NotBlank(message = "좌석 번호를 입력해주세요.")
+    @field:Pattern(regexp = "^[0-9]{1,2}\$", message = "좌석 번호는 숫자로 입력해주세요.")
     val seat : Long
 )
