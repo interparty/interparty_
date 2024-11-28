50575648e3dbac041dc9c692fcfc68245ae2c111
34-43 <jehyuck3443@gmail.com>
2024-11-28 16:31:23 +0900

    #31 fix: RedisLockService 인터페이스 명세 수정 및 구현체 Debug Logging 추가

--- src/main/kotlin/com/sparta/interparty/global/redis/RedisLockService.kt
+++ src/main/kotlin/com/sparta/interparty/global/redis/RedisLockService.kt
@@ -1,7 +1,7 @@
 package com.sparta.interparty.global.redis
 
 interface RedisLockService {
-    fun acquireLock(Key: String, expiration: Long): String?
+    fun acquireLock(key: String, expiration: Long): String?
 
-    fun releaseLock(Key: String, lockId: String)
+    fun releaseLock(key: String, lockId: String)
 }
