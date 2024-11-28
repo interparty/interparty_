af0de6fba651ee71cd8a4e3165e65cb344bdf85b
34-43 <jehyuck3443@gmail.com>
2024-11-28 16:34:24 +0900

    #31 fix: 예약 서비스의 Lock 생명주기가 Transaction 동작을 완전히 감싸도록 개선

--- src/main/kotlin/com/sparta/interparty/domain/reservation/service/ReservationService.kt
+++ src/main/kotlin/com/sparta/interparty/domain/reservation/service/ReservationService.kt
@@ -14,6 +14,8 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import org.springframework.transaction.support.TransactionSynchronization
+import org.springframework.transaction.support.TransactionSynchronizationManager
 import java.util.*
 
 
@@ -26,25 +28,22 @@ class ReservationService(
 ) {
     @Transactional
     fun createReservation(userId: UUID, showId: UUID, request: ReservationReqDto): ReservationResDto {
-        val user =
-            userRepository.findById(userId).orElseThrow { CustomException(ExceptionResponseStatus.USER_NOT_FOUND) }
-        val show =
-            showRepository.findById(showId).orElseThrow { CustomException(ExceptionResponseStatus.SHOW_NOT_FOUND) }
+        val user = userRepository.findById(userId).orElseThrow { CustomException(ExceptionResponseStatus.USER_NOT_FOUND) }
+        val show = showRepository.findById(showId).orElseThrow { CustomException(ExceptionResponseStatus.SHOW_NOT_FOUND) }
 
         // 요청 본문의 좌석 번호가 총 좌석 수 범위가 아닐 경우 예외 처리
         if (!(request.seat >= 1 && request.seat <= show.totalSeats)) {
             throw CustomException(ExceptionResponseStatus.SEAT_NOT_EXIST)
         }
 
-        // Redis Lock Key와 만료 시간 설정
+        // Redis Lock Key 와 만료 시간 설정
         val lockKey = "reservation:${showId}:${request.seat}" // 공연 ID와 좌석 번호를 포함한 고유 Key
         val lockExpiration = 10L // Lock 만료 시간 (10초)
 
         val lockId = redisLockService.acquireLock(lockKey, lockExpiration) // Redis Lock 획득
-        if (lockId == null) {
-            throw CustomException(ExceptionResponseStatus.DUPLICATE_RESERVATION) // Lock 획득 실패 시 중복 예약으로 간주
-        }
+        lockId ?: throw CustomException(ExceptionResponseStatus.DUPLICATE_RESERVATION) // Lock 획득 실패 시 중복 예약으로 간주
 
+        // Lock 획득 성공 시점부터 정합성에 영향이 가는 작업 시작
         try {
             // 현재 공연의 좌석에 대한 예약을 찾아 보고, 값이 있는 경우는 중복 예약이므로 예외 처리
             val existingReservation: Reservation? = reservationRepository.findByShowIdAndSeat(showId, request.seat)
@@ -63,16 +62,25 @@ class ReservationService(
             // 저장 및 반환
             val savedReservation = reservationRepository.save(reservation)
 
+            // Transactional 에 의한 변경사항 Commit 이 완료된 후에 여하 로직이 동작하도록 미리 설정
+            TransactionSynchronizationManager.registerSynchronization(object : TransactionSynchronization {
+                override fun afterCommit() {
+                    // Lock 은 Commit 이 완료된 후에 해제되여야 빈틈이 없음
+                    redisLockService.releaseLock(lockKey, lockId)
+                }
+            })
+
             return ReservationResDto(
                 id = savedReservation.id!!,
                 userId = savedReservation.reserver.id,
                 showId = savedReservation.show.id!!,
                 seat = savedReservation.seat,
-                status = savedReservation.status // 예약 상태
+                status = savedReservation.status
             )
-        } finally {
-            // Redis Lock 해제
+        } catch(ex: Exception) {
+            // 정상적으로 Transaction 이 Commit 될 경우 Lock 이 해제되기 때문에, 예외가 발생했을 때만 추가적으로 Lock 해제
             redisLockService.releaseLock(lockKey, lockId)
+            throw ex
         }
     }
 
