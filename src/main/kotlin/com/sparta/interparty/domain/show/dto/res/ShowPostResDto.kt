41411f77cc4fa0b0010bec8f3a49c7c36a271353
coldrice99 <coldrice99@gmail.com>
2024-11-28 13:01:41 +0900

    #32 feat: Show 엔티티에 조회수(viewCount)와 랭킹(rankings) 필드를 추가,

--- src/main/kotlin/com/sparta/interparty/domain/show/dto/res/ShowPostResDto.kt
+++ src/main/kotlin/com/sparta/interparty/domain/show/dto/res/ShowPostResDto.kt
@@ -13,5 +13,8 @@ data class ShowPostResDto(
     val price: Long = 0,
     val totalSeats: Long = 0,
     val startDateTime: String = "",
-    val category: String = ""
+    val category: String = "",
+    // Redis 캐시 추가 사항
+    val viewCount: Int = 0,
+    val rankings: MutableMap<Int, UUID>? = null
 )
